0688d26bcb6d3c5f2a1827a459cf824dc4214fa7
Rose Cox <rose@purpleprodigy.com>
2018-03-06 18:05:32 +1000

    Put primary sidebar back!

--- lib/templates/structure/sidebar.php
+++ lib/templates/structure/sidebar.php
@@ -1,14 +1,42 @@
 <?php
 
-add_action( 'widgets_init', 'ehc_remove_sidebars' );
+add_action( 'wp', 'ehc_set_up_sidebars_structure' );
 /**
- * Unregister primary and secondary sidebars.
+ * Set up the sidebar structure.
  *
  * @since 1.0.0
  *
  * @return void
  */
-function ehc_remove_sidebars() {
-	beans_deregister_widget_area( 'sidebar_primary' );
+function ehc_set_up_sidebars_structure() {
+	beans_add_attribute( 'beans_widget_panel', 'class', 'uk-panel-box' );
+	add_action( 'beans_layout_grid_settings', 'ehc_layout_grid_settings' );
+}
+
+/**
+ * Change layout settings
+ *
+ * @since 1.0.0
+ *
+ * @param $layouts
+ *
+ * @return array
+ */
+function ehc_layout_grid_settings( $layouts ) {
+	return array_merge( $layouts, array(
+		'grid'            => 10,
+		'sidebar_primary' => 3,
+	) );
+}
+
+add_action( 'widgets_init', 'ehc_remove_secondary_sidebar' );
+/**
+ * Unregister secondary sidebar.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function ehc_remove_secondary_sidebar() {
 	beans_deregister_widget_area( 'sidebar_secondary' );
 }
