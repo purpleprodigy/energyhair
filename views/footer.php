22aa8f656516d357ac329aaea98e8c3541062357
Rose Cox <rose@purpleprodigy.com>
2018-03-07 17:21:56 +1000

    Fix copyright link

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,4 @@
 <div class="uk-container-center uk-text-center">
-    <p><a class="">Copyright &copy; <?php echo date( 'Y' ); ?> <a href="/">Energy Hair Co.</a></p>
+    <p>Copyright &copy; <?php echo date( 'Y' ); ?> <a href="/">Energy Hair Co.</a></p>
     <p>Website by: <a href="https://purpleprodigy.com" target="_blank">Purple Prodigy</a></p>
 </div>
