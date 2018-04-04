5a403b7e103999f552d57c5cf2c26992e3916aeb
Rose Cox <rose@purpleprodigy.com>
2018-04-04 13:52:55 +1000

    Update Keune logo link in pre-footer

--- views/pre-footer.php
+++ views/pre-footer.php
@@ -1,7 +1,7 @@
 <div class="ehc-pre-footer-area uk-cover-background uk-flex uk-flex-center uk-flex-middle uk-overlay uk-width-1-1 uk-text-center">
     <div class="uk-overlay-panel uk-overlay-background uk-overlay-fade uk-overlay-bottom">
         <a href="http://keune.com/" target="_blank"><img
-                    src="http://77.104.150.36/~energy10/wp-content/uploads/2018/03/keune.png"
+                    src="/wp-content/uploads/2018/03/keune.png"
                     alt="Keune"></a>
         <h2>The world's most innovative and luxurious hair care, treatments and styling products from Keune.
         </h2>
